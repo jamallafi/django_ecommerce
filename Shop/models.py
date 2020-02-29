c7c4ca7c4802e0f10ec4dc43e38e68014baafb3d
dpthegrey <dpthegrey@gmail.com>
2020-03-01 00:26:31 +0530

    Created database fields

--- Shop/models.py
+++ Shop/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=222, unique=True)
+    Slug = models.SlugField(max_length=222, unique=True)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='category', blank=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=222, unique=True)
+    Slug = models.SlugField(max_length=222, unique=True)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='category', blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now_add=True)
